988e4360ea952f668cd3aafe10687a2a52355c5f
marty <marty@martyulrich.com>
2020-07-07 08:38:02 -0700

    initial commit

--- Package.swift
+++ Package.swift
@@ -18,8 +18,11 @@ let package = Package(
             dependencies: [
 				"Stripe"
 			]),
-		.binaryTarget(name: "Stripe",
-					  url: "https://github.com/stripe/stripe-ios/releases/download/v19.3.0/Stripe.xcframework.zip",
-					  checksum: "fe459dd443beee5140018388fd6933e09b8787d5b473ec9c2234d75ff0d968bd")
+		//local
+		.binaryTarget(name: "Stripe", path: "Stripe.xcframework")
+		//remote
+//		.binaryTarget(name: "Stripe",
+//					  url: "https://github.com/stripe/stripe-ios/releases/download/v19.3.0/Stripe.xcframework.zip",
+//					  checksum: "fe459dd443beee5140018388fd6933e09b8787d5b473ec9c2234d75ff0d968bd")
     ]
 )
