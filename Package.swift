827b25c3a0ab83c26128d5edc9c8787264d16d82
marty <marty@martyulrich.com>
2020-07-08 14:42:40 -0700

    updates

--- Package.swift
+++ Package.swift
@@ -4,21 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "StripePackage",
+    name: "Stripe",
     products: [
         .library(
-            name: "StripePackage",
-            targets: ["Stripe"])
+            name: "Stripe",
+            targets: ["Stripe"]),
+		.library(
+			name: "Example",
+			targets: ["Example"])
     ],
-    dependencies: [
-    ],
+    dependencies: [],
     targets: [
-		//local
-//		.binaryTarget(name: "Stripe", path: "Stripe.xcframework")
-		//remote
+		// I thought this was defining the Stripe binaryTarget...
 		.binaryTarget(name: "Stripe",
 					  url: "https://github.com/stripe/stripe-ios/releases/download/v19.3.0/Stripe.xcframework.zip",
 					  checksum: "fe459dd443beee5140018388fd6933e09b8787d5b473ec9c2234d75ff0d968bd"),
-		
+		// ... and then linking it to the Example project here via "dependencies" ...
+		.target(name: "Example", dependencies: ["Stripe"])
+		// ... so when I'm in "Example" files, I thought I'd be able to import "Stripe" to them
     ]
 )
---
+++ Sources/Example/Example.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marty Ulrich on 7/8/20.
+//
+
+import UIKit
+import Stripe
+
+class Foo: STPPaymentCardTextField {
+	
+}
